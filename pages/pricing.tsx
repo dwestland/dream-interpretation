e9af94fd848fcfc4d3b3353bf73788d4c6ee7a41
dwestland <don@westland.net>
2023-02-09 19:55:29 -0800

    Update UI

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -8,8 +8,8 @@ const PricingPage = () => (
   <Layout title="Document" description="Document description">
     <main>
       <section>
-        <h1>Start your journey today</h1>
-        <h3>Pricing for dream analysis</h3>
+        <h2 className={styles.tagLine}>Start your journey today</h2>
+        <h1 className={styles.heading}>Pricing for dream analysis</h1>
         <div className={`${styles.pricingCardContainer} pricingCard-container`}>
           <div className={styles.pricingCard}>
             <main className={styles.main}>
@@ -75,13 +75,13 @@ const PricingPage = () => (
           </div>
         </div>
 
-        <p>
+        <p className={styles.paragraph}>
           Looking to unlock the secrets hidden within your subconscious mind?
           Our dream analysis packages offer you the chance to explore your
           dreams like never before, revealing hidden insights and personal
           growth opportunities.
         </p>
-        <p>
+        <p className={styles.paragraph}>
           <strong>Starter Package: 3 Free Dreams</strong>
           <br />
           Get started with a taste of what our dream analysis has to offer.
@@ -89,7 +89,7 @@ const PricingPage = () => (
           approach to dream analysis can help you uncover new insights and
           perspectives.
         </p>
-        <p>
+        <p className={styles.paragraph}>
           <strong>Dreamer Package: 15 Dreams for $7.95</strong>
           <br />
           Take your dream analysis to the next level with the Dreamer package.
@@ -97,7 +97,7 @@ const PricingPage = () => (
           meaning, personal growth opportunities, and unlock your subconscious
           mind.
         </p>
-        <p>
+        <p className={styles.paragraph}>
           <strong>Advanced Dreamer Package: 60 Dreams for $19.95</strong>
           <br />
           Ready to dive deep into your dreams and unlock your full potential?
@@ -105,9 +105,11 @@ const PricingPage = () => (
           60 of your dreams, helping you to unlock the mysteries of your
           subconscious and achieve personal growth like never before.
         </p>
-        <p> Invest in yourself and unlock the secrets of your dreams today!</p>
-        <p></p>
-        <p>
+        <p className={styles.paragraph}>
+          Invest in yourself and unlock the secrets of your dreams today!
+        </p>
+        <p className={styles.paragraph}></p>
+        <p className={styles.paragraph}>
           * On the 1st of ever month, if you have no dreams, you will be given a
           free dream each month for life.
         </p>
