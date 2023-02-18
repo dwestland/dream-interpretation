26b227dcd0329141bedd01f5117c3207a7a54fd4
dwestland <don@westland.net>
2023-02-18 12:45:38 -0800

    Update Stripe

--- pages/thank-you.tsx
+++ pages/thank-you.tsx
@@ -5,17 +5,16 @@ export default function ThankYou() {
   return (
     <div>
       <Head>
-        <title>TomDoesTech</title>
+        <title>Thank You</title>
         <meta name="description" content="TomDoesTech" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="max-w-2xl p-4 m-auto flex">
         <div className="flex-1 p-4">
-          <h1 className="text-3xl font-bold">Sorry you had to cancel</h1>
-          <p>See you next time</p>
+          <h1 className="text-3xl font-bold">Thank you!</h1>
+          <p>Your donation has been received.</p>
         </div>
-
         <Link href="/">Home</Link>
       </main>
     </div>
---
+++ types.ts
@@ -0,0 +1,11 @@
+export interface Record {
+  id: string
+  createdTime: Date
+  fields: Fields
+}
+
+export interface Fields {
+  name: string
+  message: string
+  amount: number
+}
